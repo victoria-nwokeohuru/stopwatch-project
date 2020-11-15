ea1e09165adc52c86a20072657f821feacfa5652
victoria-nwokeohuru <vickynewman2012@gmail.com>
2020-11-15 23:35:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,74 +1,80 @@
-// Convert time to a format of hours, minutes, seconds, and milliseconds
+window.onload = function(){
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+  let msecs = 0;
 
-function timeToString(time) {
-    let diffInHrs = time / 3600000;
-    let hh = Math.floor(diffInHrs);
-  
-    let diffInMin = (diffInHrs - hh) * 60;
-    let mm = Math.floor(diffInMin);
-  
-    let diffInSec = (diffInMin - mm) * 60;
-    let ss = Math.floor(diffInSec);
-  
-    let diffInMs = (diffInSec - ss) * 100;
-    let ms = Math.floor(diffInMs);
-  
-    let formattedMM = mm.toString().padStart(2, "0");
-    let formattedSS = ss.toString().padStart(2, "0");
-    let formattedMS = ms.toString().padStart(2, "0");
-  
-    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+  let appendHours = document.getElementById('hours');
+  let appendMinutes = document.getElementById('minutes');
+  let appendSeconds = document.getElementById('seconds');
+  let appendMsecs = document.getElementById('msecs');
+
+  let start = document.getElementById('playButton');
+  let pause = document.getElementById('pauseButton');
+  let reset = document.getElementById('resetButton');
+  let Interval;
+
+  start.onclick = function(){
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+    document.getElementById('playButton').style.display = 'START';
+  };
+
+  pause.onclick = function(){
+    clearInterval(Interval);
+    let p = document.getElementById('playButton');
+    if (p.style.display === 'START'){
+      document.getElementById('playButton').innerHTML = 'RESUME';
+      p.style.display = '';
+    } else{
+      p.style.display = '';
+    }
+  };
+
+  reset.onclick = function(){
+    clearInterval(Interval);
+    hours = '00';
+    minutes = '00';
+    seconds = '00';
+    msecs = '00';    
+    document.getElementById('playButton').innerHTML = 'START';
+    appendMsecs.innerHTML = msecs;
+    appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
+    appendHours.innerHTML = hours;
+  };
+
+  function startTimer(){
+    msecs++;
+    appendMsecs.innerHTML = msecs;
+
+    if (msecs > 99){
+      console.log('seconds');
+      seconds++;
+      appendSeconds.innerHTML = seconds;
+      mSecs = 0;
+      if (seconds < 10){
+        appendSeconds.innerHTML = '0' + seconds;
+      } else{
+        appendSeconds.innerHTML = seconds;
+      }
+    };
+    if (seconds >= 60){
+      seconds = '0' + 0;
+      minutes++;
+      appendSeconds.innerHTML = seconds;
+      appendMinutes.innerHTML = minutes;
+      if (minutes < 10){
+        appendMinutes.innerHTML = '0' + minutes;
+      } else {
+        appendMinutes.innerHTML = minutes;
+      }
+    };
+    if (minutes >= 60){
+      minutes = '0' + 0;
+      hours++;
+      appendMinutes.innerHTML = minutes;
+      appendHours.innerHTML = hours;
+    }
   }
-  
-  // Declare variables to use in our functions below
-  
-  let startTime;
-  let elapsedTime = 0;
-  let timerInterval;
-  
-  // Create function to modify innerHTML
-  
-  function print(txt) {
-    document.getElementById("display").innerHTML = txt;
-  }
-  
-  // Create "start", "pause" and "reset" functions
-  
-  function start() {
-    startTime = Date.now() - elapsedTime;
-    timerInterval = setInterval(function printTime() {
-      elapsedTime = Date.now() - startTime;
-      print(timeToString(elapsedTime));
-    }, 10);
-    showButton("PAUSE");
-  }
-  
-  function pause() {
-    clearInterval(timerInterval);
-    showButton("PLAY");
-  }
-  
-  function reset() {
-    clearInterval(timerInterval);
-    print("00:00:00");
-    elapsedTime = 0;
-    showButton("PLAY");
-  }
-  
-  // Create function to display buttons
-  
-  function showButton(buttonKey) {
-    const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
-    const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
-    buttonToShow.style.display = "block";
-    buttonToHide.style.display = "none";
-  }
-  // Create event listeners
-  
-  let playButton = document.getElementById("playButton");
-  let pauseButton = document.getElementById("pauseButton");
-  let resetButton = document.getElementById("resetButton");
-  
-  playButton.addEventListener("click", start);
-  pauseButton.addEventListener("click", pause);
-  resetButton.addEventListener("click", reset);
+};
